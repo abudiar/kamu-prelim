f52e5fe1f12948a9cbec73e35239a066a309b907
Alan Budiarto <alanwbudiarto@gmail.com>
2020-07-09 02:37:24 +0700

    Nuxt Vue Setup

---
+++ client/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+html {
+  overflow: hidden;
+}
+</style>
---
+++ client/pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container class="fill-height" fluid>
+    <v-card>
+      <v-card-title class="headline">
+        Welcome to the Vuetify + Nuxt.js template
+      </v-card-title>
+      <v-card-text>
+        <p>
+          Vuetify is a progressive Material Design component framework for
+          Vue.js. It was designed to empower developers to create amazing
+          applications.
+        </p>
+        <p>
+          For more information on Vuetify, check out the
+          <a
+            href="https://vuetifyjs.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            documentation </a
+          >.
+        </p>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+};
+</script>
